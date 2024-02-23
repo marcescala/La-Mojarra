942ee9a9e7c4282fd5c7a4ec6cdad2f8199709da
Laura Hincapie <laura@pulpowms.com>
2024-02-23 17:03:52 -0500

    init project

---
+++ web_app/src/app/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+         <img src="/../assets/header_image.jpeg" alt="Header Image" />
+
+          {/* Add any additional content for your header */}
+        </header>
+      );
+}
+  
