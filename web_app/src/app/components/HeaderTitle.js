12ec077a41801ee52587d5c6bbaa8341b6e2992c
Laura Hincapie <laura@pulpowms.com>
2024-02-24 12:42:04 -0500

    init project

---
+++ web_app/src/app/components/HeaderTitle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from 'next/image'; // Import Next.js Image component
+
+function HeaderTitle() {
+  return (
+    <header className="header-title">
+      <h1>LA MOJARRA NEWS</h1>
+    </header>
+  );
+}
+
+export default HeaderTitle;
